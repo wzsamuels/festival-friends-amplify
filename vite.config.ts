2c01f4c191f8d3ea667edb6ab3fb2c240ab86620
wzsamuels <wzsamuels@gmail.com>
2023-04-12 00:04:10 -0400

    More vite tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: {}, //fix dev build
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis", //<-- AWS SDK
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill(),
+      ],
+    },
   },
   resolve: {
     alias: {
-      "./runtimeConfig": "./runtimeConfig.browser", //fix production build
+      './runtimeConfig': './runtimeConfig.browser', // <-- Fix from above
     },
   },
   test: {
