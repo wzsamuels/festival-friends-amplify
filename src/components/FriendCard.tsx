b48dae78cd89925030fbe677dfda423813b2a377
wzsamuels <wzsamuels@gmail.com>
2023-04-20 23:54:10 -0400

    Fix spacing of Friend Card

--- src/components/FriendCard.tsx
+++ src/components/FriendCard.tsx
@@ -22,7 +22,7 @@ const FriendCard = ({profile, link, onClick} : FriendCardProps) => {
   }, [])
 
   return (
-    <IonItem onClick={onClick} button={true} lines='none' {...(link ? profileUrl : {})} className={'max-w-[400px] m-4 p-2 rounded-xl hover:border-gray-500 shadow-xl'}>
+    <IonItem onClick={onClick} button={true} lines='none' {...(link ? profileUrl : {})} className={'max-w-[400px] min-w-[300px] m-4 p-2 rounded-xl hover:border-gray-500 shadow-xl'}>
       <div className={'mx-2'}>
         <img className={'rounded-full'} width={100} height={100} src={profileImage} alt={`${profile.firstName} ${profile.lastName}'s Profile Image`}/>
       </div>
