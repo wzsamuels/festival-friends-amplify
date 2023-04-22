94fe79ecdb1563b64013dfcc5f7f18272c9e1e46
wzsamuels <wzsamuels@gmail.com>
2023-04-22 17:07:44 -0400

    Moved all modal components to their own files

--- src/pages/Account/ProfileVerified.tsx
+++ src/pages/Account/ProfileVerified.tsx
@@ -1,46 +1,27 @@
 import {Photo, UserProfile} from "../../models";
-import React, {FormEvent, useEffect, useState} from "react";
-import { SubmitHandler} from "react-hook-form";
+import React, {useEffect, useState} from "react";
 import {Storage} from "aws-amplify";
 import {DataStore} from "@aws-amplify/datastore";
-import getErrorMessage from "../../lib/getErrorMessage";
-import {IonAlert, IonButton, IonIcon} from "@ionic/react";
-import {AccountSettings} from "@aws-amplify/ui-react";
-import ProfileForm from "../../components/ProfileForm";
-import {ProfileInputs} from "../../types";
-import { v4 as uuidv4 } from 'uuid';
+import {
+  IonAlert,
+  IonButton,
+  IonIcon
+} from "@ionic/react";
 import {personCircle} from "ionicons/icons";
+import ProfileEditModal from "./ProfileEditModal";
+import ProfileImageModal from "./ProfileImageModal";
+import PhotoUploadModal from "./PhotoUploadModal";
+import PhotoImage from "../../components/PhotoImage";
 
 const ProfileVerified = ({username, profile} : {username: string, profile: UserProfile}) => {
   const [profileImage, setProfileImage] = useState("")
-  const [selectedFile, setSelectedFile] = useState<File>()
-  const [message, setMessage] = useState("")
   const [photos, setPhotos] = useState<Photo[]>([])
-
-  const handleFileSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    if(selectedFile) {
-      console.log(selectedFile)
-      try {
-        await Storage.put(`${username}/${selectedFile.name}`, selectedFile, {contentType: selectedFile.type});
-        // Delete old image if it exists
-        if(profile?.profileImage) {
-          await Storage.remove(profile?.profileImage)
-        }
-        // Profile should always exist, but let's make Typescript happy
-        if(profile) {
-          await DataStore.save(UserProfile.copyOf(profile, updated => {
-            updated.profileImage = `${username}/${selectedFile.name}`
-          }))
-        }
-      } catch (error) {
-        console.log("Error uploading file: ", error);
-      }
-    }
-    else {
-      alert("No file selected!")
-    }
-  }
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+  const [isProfileModalOpen, setProfileModalOpen] = useState(false)
+  const [isPhotoModalOpen, setPhotoModalOpen] = useState(false)
+  const [isProfileImageModalOpen, setIsProfileImageModalOpen] = useState(false)
+  const [isAlertOpen, setAlertOpen] = useState(false)
+  const [alertMessage, setAlertMessage] = useState("")
 
   useEffect(() => {
     const fetchProfileImage = async () => {
@@ -53,12 +34,9 @@ const ProfileVerified = ({username, profile} : {username: string, profile: UserP
         setProfileImage(image);
       }
     }
+
     fetchProfileImage()
-    const fetchPhotos = async () => {
-      const photos = await DataStore.query(Photo, photo => photo.userProfileID.eq(profile.id))
-      console.log(photos)
-      setPhotos(photos)
-    }
+
     const photoSub = DataStore.observeQuery(Photo, photo => photo.userProfileID.eq(profile.id)).subscribe(({items}) => {
       setPhotos(items)
     })
@@ -68,58 +46,26 @@ const ProfileVerified = ({username, profile} : {username: string, profile: UserP
 
   }, [profile])
 
-  const handlePhotoUpload = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log(selectedFile)
-    if(selectedFile) {
-      const id = uuidv4()
-      try {
-        await Storage.put(`${username}/${id}`, selectedFile, {contentType: selectedFile.type});
-        // Profile should always exist, but let's make Typescript happy
-        if(profile) {
-          await DataStore.save(new Photo({userProfile: profile, s3Key: `${username}/${id}`, isPrivate: false, userProfileID: profile.id}))
-        }
-      } catch (error) {
-        console.log("Error uploading file: ", error);
-      }
-    }
-    else {
-      alert("No file selected!")
-    }
-  }
 
-  const handleProfileUpdate: SubmitHandler<ProfileInputs> = async (data) => {
-    console.log(data)
-    try {
-      const results = await DataStore.save(UserProfile.copyOf(profile, updated => {
-        updated.firstName = data.firstName
-        updated.lastName = data.lastName
-        updated.phone = data.phone
-        updated.city = data.city
-        updated.state = data.state
-        updated.zipcode = data.zipcode
-        updated.address = data.address
-        updated.address2 = data.address2
-        updated.school = data.school
-      }))
-      console.log(results)
-      setMessage("Profile successfully updated!")
-    } catch (error) {
-      setMessage(`Error updating profile: ${getErrorMessage(error)}`)
+
+  useEffect(() => {
+    if (!selectedFile) {
+      return
     }
-  }
+    setPhotoModalOpen(true);
+  }, [selectedFile])
 
 
   return (
     <div className='flex flex-col items-center p-4 mt-8 w-full'>
-      <section className={'my-8 flex justify-center flex-col'}>
+      <section className={'flex justify-center flex flex-col'}>
         {
           profile.profileImage ?
-            <img id='change-alert' className='max-w-[350px] rounded-full cursor-pointer rounded-xl' src={profileImage} alt="Profile Image"/>
+            <img id='change-alert' className='max-w-[350px] rounded-full cursor-pointer' src={profileImage} alt="Profile Image"/>
             :
-            <div className='w-[350px] h-[350px] border border-medium-default rounded-xl'>
+            <div className='w-[350px] h-[350px] border border-medium-default'>
               <IonIcon icon={personCircle} className='w-full h-full text-medium-default'/>
-              </div>
+            </div>
         }
         <IonAlert
           header='Change Profile Picture'
@@ -136,72 +82,66 @@ const ProfileVerified = ({username, profile} : {username: string, profile: UserP
           ]}
         >
         </IonAlert>
-        <form className='flex flex-col justify-center w-full' onSubmit={handleFileSubmit}>
-          <input
-            type='file'
-            accept="image/png, image/jpeg"
-            onChange={e => e?.target?.files && setSelectedFile(e.target.files[0])}
-            className='my-4'
-          />
-          <div className='mt-6 flex justify-center'>
-            <IonButton type="submit">Update Profile Image</IonButton>
-          </div>
-        </form>
-      </section>
-      <section className='my-8 min-w-4xl  max-w-[600px] w-full'>
-        <h2 className='text-2xl md:text-3xl my-6'>Update Profile</h2>
-        <ProfileForm onSubmit={handleProfileUpdate} profile={profile}/>
-        { message }
-      </section>
-      <section>
-        <h2>Photos</h2>
-        <form onSubmit={handlePhotoUpload}>
 
+        <div className='flex justify-center my-2'>
+          <IonButton onClick={() => setProfileModalOpen(true)}>
+            Edit Profile
+          </IonButton>
+        </div>
+
+      </section>
+      <hr className='my-8 border border-primary-default w-full'/>
+      <section >
+        <div className='flex items-center justify-between my-4'>
+          <h2 className='text-2xl'>Photos</h2>
+          <label htmlFor="upload-photo" className='block'><IonButton class='pointer-events-none'>Upload Photo</IonButton></label>
           <input type="file" accept="image/png, image/jpeg"
                  onChange={e => e?.target?.files && setSelectedFile(e.target.files[0])}
-                 className='my-4'/>
-          <IonButton type='submit'>Upload Photo</IonButton>
-        </form>
-        <div>
+                 className='my-4 hidden'
+                 id="upload-photo"/>
+        </div>
+        <div className='flex justify-center gap-4 flex-wrap w-full my-8'>
           {
-            photos?.map(photo => <PhotoImage key={photo.id} photo={photo}/>)
+            photos?.map(photo =>
+              <div className='max-w-[200px] max-h-[200px]' key={photo.id}>
+                <PhotoImage className='object-cover cursor-pointer'  photo={photo}/>
+              </div>
+            )
           }
         </div>
       </section>
-      <section className='my-8 min-w-4xl  max-w-[600px] w-full'>
-        <h2 className='text-2xl md:text-3xl my-6'>Change Password</h2>
-        <AccountSettings.ChangePassword/>
-      </section>
-      <section className='my-8 min-w-4xl  max-w-[600px] w-full'>
-        <h2 className='text-2xl md:text-3xl my-6'>Delete Account</h2>
-        <div className='my-6 flex justify-center'>
-          <AccountSettings.DeleteUser/>
-        </div>
-        <h3 className='text-danger-default my-4 text-xl text-center'>This action cannot be undone!</h3>
-      </section>
+      <IonAlert
+        isOpen={isAlertOpen}
+        header={alertMessage}
+        buttons={['OK']}
+        onDidDismiss={() => setAlertOpen(false)}
+      ></IonAlert>
+      <PhotoUploadModal
+        profile={profile}
+        username={username}
+        isOpen={isPhotoModalOpen}
+        setIsOpen={setPhotoModalOpen}
+        photoFile={selectedFile}
+        setPhotoFile={setSelectedFile}
+      />
+      <ProfileImageModal
+        profile={profile}
+        username={username}
+        isOpen={isProfileImageModalOpen}
+        setIsOpen={setIsProfileImageModalOpen}
+        photos={photos}/>
+      <ProfileEditModal
+        profile={profile}
+        profileImage={profileImage}
+        isOpen={isProfileModalOpen}
+        setIsOpen={setProfileModalOpen}
+        username={username}
+        callback={() => {
+          setProfileModalOpen(false);
+          setIsProfileImageModalOpen(true);
+        }}
+      />
     </div>
-  )
-}
-
-const PhotoImage = ({photo} : {photo: Photo}) => {
-  const [photoUrl, setPhotoUrl] = useState('')
-  console.log(photo)
-
-  useEffect(() => {
-    if(photo) {
-      Storage.get(photo.s3Key, {
-        level: "public"
-      }).then(url => {
-        setPhotoUrl(url)
-      })
-    }
-  }, [photo])
-
-  if(!photo) {
-    return null;
-  }
-  return (
-    <img className='max-w-[350px] rounded-full cursor-pointer rounded-xl' src={photoUrl} alt={`${photo.description ? photo.description : "Photo"}`}/>
   )
 }
 
