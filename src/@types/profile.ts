f1bb417c0a2cda289ca1b6a06580bafefe852fbd
wzsamuels <wzsamuels@gmail.com>
2023-04-22 16:57:29 -0400

    Add ProfileModalProp type shared by various profile page modals

---
+++ src/@types/profile.ts
@@ -0,0 +1,14 @@
+import {Photo, UserProfile} from "../models";
+
+
+
+
+
+export interface ProfileModalProps {
+  profile: UserProfile
+  profileImage?: string
+  username: string
+  isOpen: boolean
+  setIsOpen: (isOpen: boolean) => void
+  callback?: () => void
+}
