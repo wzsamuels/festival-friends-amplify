943ef83fe3669b76727d3da912d4d3c636f0fea8
wzsamuels <wzsamuels@gmail.com>
2023-04-26 11:38:47 -0400

    Add Rides and Groups ages, UserProfileProvider

--- src/App.tsx
+++ src/App.tsx
@@ -36,76 +36,90 @@ import './theme/variables.css';
 import './index.css'
 
 import ProfilePage from './pages/Profile';
-import React, {useState} from "react";
+import React, { useState} from "react";
 import Admin from "./pages/Admin";
 import FriendsPage from "./pages/Friends";
 import Events from "./pages/Events";
-import {chatboxEllipses, musicalNotes, people, settings} from "ionicons/icons";
+import {car, chatboxEllipses, happyOutline, musicalNotes, people} from "ionicons/icons";
 import DataStoreContext from './context/DataStoreContext';
 import AccountSettingsPage from "./pages/Account/AccountSettings";
 import EventDetailPage from "./pages/Events/EventDetail";
 import ImageProvider from "./context/ImageProvider";
+import RidesPage from "./pages/Rides";
+import GroupsPage from "./pages/Groups";
+import UserProfileProvider from "./context/UserProfileProvider";
 
 setupIonicReact();
 
 const App: React.FC = () => {
   const [dataStoreCleared, setDataStoreCleared] = useState(true);
-
   const saveDataStoreCleared = (value: boolean) => {
     setDataStoreCleared(value)
   }
 
   return (
     <DataStoreContext.Provider value={{ dataStoreCleared, saveDataStoreCleared }}>
-      <ImageProvider>
-        <IonApp>
-          <IonReactRouter>
-            <IonTabs>
-              <IonRouterOutlet>
-                <Route exact path="/events" render={() => <Events/>}>
-                </Route>
-                <Route path='/events/:id' component={EventDetailPage}/>
-                <Route exact path="/friends">
-                  <FriendsPage />
-                </Route>
-                <Route exact path="/messages">
-                  <MessagePage />
-                </Route>
-                <Route exact path='/account'>
-                  <AccountPage/>
-                </Route>
-                <Route path='/account/settings'>
-                  <AccountSettingsPage/>
-                </Route>
-                <Route path='/admin'>
-                  <Admin/>
-                </Route>
-                <Route path='/friends/profile/:id' component={ProfilePage}/>
-                <Redirect exact from="/" to="/events"  />
+      <UserProfileProvider>
+        <ImageProvider>
+          <IonApp>
+            <IonReactRouter>
+              <IonTabs>
+                <IonRouterOutlet>
+                  <Route exact path="/events" render={() => <Events />}>
+                  </Route>
+                  <Route path='/events/:id' component={EventDetailPage}/>
+                  <Route exact path="/friends">
+                    <FriendsPage />
+                  </Route>
+                  <Route exact path="/messages">
+                    <MessagePage />
+                  </Route>
+                  <Route exact path='/account'>
+                    <AccountPage />
+                  </Route>
+                  <Route exact path='/rides'>
+                    <RidesPage/>
+                  </Route>
+                  <Route exact path='/groups'>
+                    <GroupsPage/>
+                  </Route>
+                  <Route path='/account/settings'>
+                    <AccountSettingsPage/>
+                  </Route>
+                  <Route path='/admin'>
+                    <Admin/>
+                  </Route>
+                  <Route path='/friends/profile/:id' component={ProfilePage}/>
+                  <Redirect exact from="/" to="/events"  />
 
-              </IonRouterOutlet>
-              <IonTabBar slot="bottom">
-                <IonTabButton tab="tab1" href="/events">
-                  <IonIcon aria-hidden="true" icon={musicalNotes} />
-                  <IonLabel>Events</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="tab2" href="/friends">
-                  <IonIcon aria-hidden="true" icon={people} />
-                  <IonLabel>Friends</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="tab3" href="/messages">
-                  <IonIcon aria-hidden="true" icon={chatboxEllipses} />
-                  <IonLabel>Messages</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="tab4" href="/admin">
-                  <IonIcon aria-hidden="true" icon={settings} />
-                  <IonLabel>Admin</IonLabel>
-                </IonTabButton>
-              </IonTabBar>
-            </IonTabs>
-          </IonReactRouter>
-        </IonApp>
-      </ImageProvider>
+                </IonRouterOutlet>
+                <IonTabBar slot="bottom">
+                  <IonTabButton tab="tab1" href="/events">
+                    <IonIcon aria-hidden="true" icon={musicalNotes} />
+                    <IonLabel>Events</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab="tab2" href="/friends">
+                    <IonIcon aria-hidden="true" icon={happyOutline} />
+                    <IonLabel>Friends</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab="tab3" href="/messages">
+                    <IonIcon aria-hidden="true" icon={chatboxEllipses} />
+                    <IonLabel>Messages</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab="tab4" href="/groups">
+                    <IonIcon aria-hidden="true" icon={people} />
+                    <IonLabel>Groups</IonLabel>
+                  </IonTabButton>
+                  <IonTabButton tab="tab5" href="/rides">
+                    <IonIcon aria-hidden="true" icon={car} />
+                    <IonLabel>Rides</IonLabel>
+                  </IonTabButton>
+                </IonTabBar>
+              </IonTabs>
+            </IonReactRouter>
+          </IonApp>
+        </ImageProvider>
+      </UserProfileProvider>
     </DataStoreContext.Provider>
   );
 }
