f3d7d34e81389e7db9e8b409c5b744b26120fefa
wzsamuels <wzsamuels@gmail.com>
2023-05-12 23:18:51 -0400

    Fix form styles and add new phone custom hook

--- src/components/pages/Profile/ProfileUnverified.tsx
+++ src/components/pages/Profile/ProfileUnverified.tsx
@@ -1,5 +1,5 @@
-import {SubmitHandler, useForm} from "react-hook-form";
-import React from "react";
+import {SubmitHandler, useForm, Controller } from "react-hook-form";
+import React, {useRef, useState} from "react";
 import {CollegeGroup, UserProfile} from "../../../models";
 import {DataStore} from "@aws-amplify/datastore";
 import {ProfileInputs} from "../../../types";
@@ -8,16 +8,23 @@ import InputWrapper from "../../common/InputWrapper/InputWrapper";
 import Input from "../../common/Input/Input";
 import Label from "../../common/Label/Label";
 import {useUserProfileStore} from "../../../stores/friendProfilesStore";
+import Button from "../../common/Button/Button";
+import states from "../../../data/states";
+import {useFormattedPhoneInput} from "../../../hooks/useFormattedPhoneInput";
 
 const ProfileUnverified = () => {
   const { user } = useAuthenticator((context) => [context.user]);
   const userProfile = useUserProfileStore(state => state.userProfile)
-  const { register, handleSubmit} = useForm<ProfileInputs>({
+  const { phone, inputRef, handlePhoneChange } = useFormattedPhoneInput();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { register, handleSubmit, control} = useForm<ProfileInputs>({
+
     defaultValues: {
       firstName: userProfile?.firstName || "",
       lastName: userProfile?.lastName || "",
       username: userProfile?.username || "",
-      phone: userProfile?.phone || "",
       school: userProfile?.school || "",
       city: userProfile?.city || "",
       state: userProfile?.state || "",
@@ -30,8 +37,8 @@ const ProfileUnverified = () => {
   const email = user?.attributes?.email as string;
 
   const createNewProfile: SubmitHandler<ProfileInputs> = async data => {
-    alert(`Email: ${email}\nUsername: ${username}\nData: ${JSON.stringify(data)}`)
-
+    //alert(`Email: ${email}\nUsername: ${username}\nData: ${JSON.stringify(data)}`)
+    setIsSubmitting(true);
     let collegeGroup: CollegeGroup | null = null;
     if(email?.endsWith('.edu')) {
       const emailDomain = email.split('@')[1]
@@ -43,57 +50,96 @@ const ProfileUnverified = () => {
       console.log("Creating profile from data:", data)
       const newProfile = await DataStore.save(
         new UserProfile({
+          ...data,
           "verified": false,
           "verifySubmitted": true,
           "userID": username,
           "email": email,
-          ...data,
+          "phone": phone,
           ...(collegeGroup && {collegeGroup: collegeGroup}),
         })
       );
       console.log(newProfile);
     } catch(err) {
       console.error("Error saving profile:", err)
+    } finally {
+      setIsSubmitting(false);
+      setIsSubmitted(true);
     }
   }
 
+  if(isSubmitted) {
+    return (
+      <div className='flex flex-col justify-center items-center mt-4 p-4'>
+        <h1 className='text-xl md:text-2xl'>Profile Created!</h1>
+        <p className='my-6'>Your profile has been submitted for verification. You&asop;ll receive an email at the address you used to sign up for this account once the process has been completed.</p>
+        <p>If you&asop;ve received confirmation that your profile has been verified but are still seeing this message, please try refreshing this page.</p>
+      </div>
+    )
+  }
+
   return (
-    <div className='flex flex-col justify-center items-center p-4'>
+    <div className='flex flex-col justify-center items-center mt-4 p-4'>
       <h1 className='text-xl md:text-2xl'>Create Profile</h1>
       <p className='my-6'>This personal information will be used to verify your identity for the safety of our community. Once your profile is verified, all your information will be set to private until you specify otherwise.</p>
-      <form className='flex flex-col w-full' onSubmit={handleSubmit(createNewProfile)}>
-        <div className='flex flex-wrap'>
-          <label className='basis-[150px]'>First Name</label>
-          <input className='border-b  border-b-medium-tint focus:border-b-primary-default focus:outline-0' {...register("firstName")}/>
-        </div>
-        <div className='flex flex-wrap'>
-          <label className='basis-[150px]'>Last Name</label>
-          <input className='border-b border-b-medium-tint focus:border-b-primary-default focus:outline-0' {...register("lastName")}/>
-        </div>
-        <div className='flex flex-wrap'>
-          <label className='basis-[150px]'>Phone</label>
-          <input className='border-b border-b-medium-tint focus:border-b-primary-default focus:outline-0' {...register("phone")}/>
-        </div>
-        <InputWrapper>
-          <Label>City</Label>
-          <Input {...register('city')} name="city"/>
-        </InputWrapper>
-        <InputWrapper>
-          <Label>State</Label>
-          <Input {...register('state')} name="state"/>
-        </InputWrapper>
-        <div className='flex flex-wrap'>
-          <label className='basis-[150px]'>Address</label>
-          <input className='border-b border-b-medium-tint focus:border-b-primary-default focus:outline-0' {...register("address")}/>
-        </div>
-        <div className='flex flex-wrap'>
-          <label className='basis-[150px]'>Address 2</label>
-          <input className='border-b border-b-medium-tint focus:border-b-primary-default focus:outline-0' {...register("address2")}/>
-        </div>
-        <div className='flex justify-center items-center my-6'>
-          <button className='bg-primary-default text-light-default rounded-md px-8 py-2' type="submit">Submit</button>
-        </div>
-      </form>
+      <div className='flex justify-center w-full'>
+        <form className='w-full max-w-4xl' onSubmit={handleSubmit(createNewProfile)}>
+          <div className='flex flex-col md:flex-row'>
+            <InputWrapper className='my-4 flex-1'>
+              <Label className=''>First Name</Label>
+              <Input className='mr-2' {...register("firstName", {required: true})}/>
+            </InputWrapper>
+            <InputWrapper className='my-4 flex-1'>
+              <Label>Last Name</Label>
+             <Input {...register("lastName", {required: true})}/>
+            </InputWrapper>
+          </div>
+          <div className='flex flex-col md:flex-row'>
+            <InputWrapper className='my-4 flex-1'>
+              <Label>City</Label>
+              <Input {...register('city', {required: true})} name="city"/>
+            </InputWrapper>
+            <InputWrapper className='my-4 flex-1'>
+              <Label>State</Label>
+              <select placeholder=''  {...register('state', {required: true})} name="state">
+                <option value="" disabled selected>Select state</option>
+                {states.map((state) => (
+                  <option key={state.code} value={state.name}>{state.name}</option>
+                ))}
+              </select>
+            </InputWrapper>
+          </div>
+          <InputWrapper className='my-4'>
+            <label className='basis-[150px]'>Address</label>
+            <Input {...register("address", {required: true})}/>
+          </InputWrapper>
+          <InputWrapper className='my-4'>
+            <label className='basis-[150px]'>Address 2</label>
+            <Input {...register("address2")}/>
+          </InputWrapper>
+          <InputWrapper className='my-4'>
+            <label className='basis-[150px]'>Phone</label>
+            <Controller
+              name="phone"
+              control={control}
+              defaultValue=""
+              render={({ field }) =>
+                <Input
+                  type="tel"
+                  {...field}
+                  value={phone}
+                  required
+                  onChange={handlePhoneChange}
+                  ref={inputRef}
+                />
+              }
+            />
+          </InputWrapper>
+          <div className='flex justify-center items-center my-6'>
+            <Button className='bg-primary-default text-light-default rounded-md px-8 py-2' type="submit">Submit</Button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
