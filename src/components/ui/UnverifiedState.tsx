746a4a879f1f78120fdc0a11ac7e1605779afddf
wzsamuels <wzsamuels@gmail.com>
2023-05-13 18:02:45 -0400

    Refactored shared auth state UIs to separate components

---
+++ src/components/ui/UnverifiedState.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UnverifiedState: React.FC = () => (
+  <div className='m-4 text-xl '>Your profile is not verified yet. Once your profile has been verified, you will be able to access this feature.</div>
+);
+
+export default UnverifiedState;
