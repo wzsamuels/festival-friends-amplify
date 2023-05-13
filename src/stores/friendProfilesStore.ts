4e9a9fb43447efd831b8d88ce68492b7ed56757a
wzsamuels <wzsamuels@gmail.com>
2023-05-12 23:15:45 -0400

    Fix loadingFriendsProfile bug

--- src/stores/friendProfilesStore.ts
+++ src/stores/friendProfilesStore.ts
@@ -67,7 +67,7 @@ const friendProfilesStore = create(
             friendsSub.unsubscribe();
           };
         } else {
-          set({loadingUserProfile: false })
+          set({loadingUserProfile: false, loadingFriendProfiles: false })
         }
       },
       reset: () => {
