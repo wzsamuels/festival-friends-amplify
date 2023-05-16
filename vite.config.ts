6f49814d7fe8f69e677fddf7efc10c36928eef17
wzsamuels <wzsamuels@gmail.com>
2023-05-16 16:03:33 -0400

    Remove typo

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 // https://vitejs.dev/config/
-// @ts-ignore
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
