9e1d21857af796374b072943ce1f464ab99c94d4
wzsamuels <wzsamuels@gmail.com>
2023-05-24 18:42:43 -0400

    Update getSignedUrl to use S3 access levels and identityId

--- src/context/ImageProvider.tsx
+++ src/context/ImageProvider.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import { Storage } from "aws-amplify";
 import ImageContext from "./ImageContext";
+import getErrorMessage from "../lib/getErrorMessage";
+import {S3Levels} from "../@types/s3";
 
 interface ImageProviderProps {
   children: React.ReactNode;
@@ -11,14 +13,22 @@ const ImageProvider = ({ children }: ImageProviderProps) => {
   const [imageURLs, setImageURLs] = useState<{ [key: string]: string }>({});
 
   // Only fetch the signed URL once per image
-  const getSignedURL = async (s3Key: string) => {
+  const getSignedURL = async (s3Key: string, level: S3Levels = 'public', identityId?: string) => {
+    // Check if the image key has already been fetched and stored in the state.
+    // If not, fetch it and store it in the state.
+    // This is to avoid fetching the same image key multiple times.
     if (!imageURLs[s3Key]) {
-      const signedURL = await Storage.get(s3Key, {
-        level: "public",
-        expires: 60 * 60 * 24,
-      });
-      setImageURLs((prevState) => ({ ...prevState, [s3Key]: signedURL }));
-      return signedURL;
+      try {
+        const signedURL = await Storage.get(s3Key, {
+          level: level,
+          expires: 60 * 60 * 24,
+          identityId: identityId
+        });
+        setImageURLs((prevState) => ({ ...prevState, [s3Key]: signedURL }));
+        return signedURL;
+      } catch (e) {
+        console.log("Error fetching signed URL", getErrorMessage(e));
+      }
     }
     return imageURLs[s3Key];
   };
