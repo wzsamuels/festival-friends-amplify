20ca2aaefa3cf3f4db927fe2707333b66748af2c
wzsamuels <wzsamuels@gmail.com>
2023-05-24 18:17:44 -0400

    Add type for S3 access levels

---
+++ src/@types/s3.ts
@@ -0,0 +1,1 @@
+export type S3Levels = 'public' | 'private' | 'protected';
--- src/context/ImageContext.tsx
+++ src/context/ImageContext.tsx
@@ -1,9 +1,10 @@
 // ImageContext.tsx
 import { createContext } from "react";
+import {S3Levels} from "../@types/s3";
 
 interface ImageContextData {
   imageURLs: { [key: string]: string };
-  getSignedURL: (s3Key: string) => Promise<string>;
+  getSignedURL: (s3Key: string, level: S3Levels, identityId?: string) => Promise<string>;
 }
 
 const ImageContext = createContext<ImageContextData>({} as ImageContextData);
