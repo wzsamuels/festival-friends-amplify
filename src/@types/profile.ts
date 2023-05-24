382b863b1950539e39a1b830b2fdccaab494dfb2
wzsamuels <wzsamuels@gmail.com>
2023-05-24 18:34:07 -0400

    Renamed "username" to "sub" for profile types

--- src/@types/profile.ts
+++ src/@types/profile.ts
@@ -3,7 +3,7 @@ import { UserProfile } from "../models";
 export interface ProfileModalProps {
   profile: UserProfile;
   profileImage?: string;
-  username: string;
+  sub: string;
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
   callback?: () => void;
