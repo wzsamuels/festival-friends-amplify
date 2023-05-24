aa9c3195cb9c669bb540214b80a4ffbe8a066926
wzsamuels <wzsamuels@gmail.com>
2023-05-24 18:14:20 -0400

    Add event store for managing event state

---
+++ src/stores/eventStore.ts
@@ -0,0 +1,65 @@
+import {create, SetState} from "zustand";
+import {Festival} from "../models";
+import getErrorMessage from "../lib/getErrorMessage";
+import {API, DataStore, graphqlOperation} from "aws-amplify";
+import {GraphQLResult} from "@aws-amplify/api";
+import {listFestivals} from "../graphql/queries";
+
+type EventStore = {
+  events: Festival[],
+  loadingEvents: boolean,
+  fetchEvents: () => void
+  error: string | null,
+}
+
+type GraphQLResultExtended = GraphQLResult & {
+  data: {
+    listFestivals: {
+      items: Festival[];
+    };
+  };
+  errors?: { message: string }[]
+};
+
+const useEventStore = create<EventStore>((set: SetState<EventStore>) => ({
+  events: [] as Festival[],
+  loadingEvents: false,
+  error: null,
+  fetchEvents: async () => {
+    console.log("Fetching events in store")
+    set({loadingEvents: true})
+    try {
+
+      /*
+      const eventSub = await DataStore.observeQuery(Festival).subscribe(({items}) => {
+        console.log("Events in store", items)
+        set({events: items})
+      })
+
+       */
+
+      const eventData = await DataStore.query(Festival);
+      set({events: eventData})
+
+      /*
+      const eventData = await API.graphql(graphqlOperation(listFestivals)) as GraphQLResultExtended;
+      if(eventData.errors) {
+        set({error: eventData.errors[0].message || "Unknown error"})
+      } else {
+        const events = eventData.data.listFestivals.items
+        console.log("Events in store", events)
+        set({events})
+      }
+
+       */
+
+
+    } catch (e) {
+      console.log("Error observing events in EventPage.tsx", getErrorMessage(e));
+    } finally {
+      set({loadingEvents: false})
+    }
+  }
+}));
+
+export default useEventStore;
