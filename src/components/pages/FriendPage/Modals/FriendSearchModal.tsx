fdab8615c7b5ca3f6c2f5db50658c796ccb941ce
wzsamuels <wzsamuels@gmail.com>
2023-05-25 00:38:41 -0400

    Add searching ui feedback to friend search modal

--- src/components/pages/FriendPage/Modals/FriendSearchModal.tsx
+++ src/components/pages/FriendPage/Modals/FriendSearchModal.tsx
@@ -8,13 +8,14 @@ import Label from "../../../common/Label/Label";
 import Input from "../../../common/Input/Input";
 import Button from "../../../common/Button/Button";
 import CustomAlert from "../../../common/Alert/Alert";
-import { useUserProfileStore } from "../../../../stores/friendProfilesStore";
 import Toast from "../../../common/Toast/Toast";
 import { ToastData } from "../../../../types";
 import { criteria, getFilteredData } from "../../../../lib/searchHelpers";
 import { createFriendRequest } from "../../../../services/FriendsService";
 import InputWrapper from "../../../common/InputWrapper/InputWrapper";
 import states from "../../../../data/states";
+import useProfileStore from "../../../../stores/profileStore";
+import Select from "../../../common/Select";
 
 interface SearchInput {
   firstName?: string;
@@ -34,15 +35,18 @@ const FriendSearchModal = ({ isOpen, setIsOpen }: FriendSearchModalProps) => {
   const [results, setResults] = useState<UserProfile[]>([]);
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
   const [currentResult, setCurrentResult] = useState<UserProfile | null>(null);
-  const { userProfile } = useUserProfileStore();
+  const userProfile = useProfileStore((state) => state.userProfile);
   const [toastData, setToastData] = useState<ToastData | null>(null);
+  const [isSearching, setIsSearching] = useState(false);
+  const [hasSearched, setHasSearched] = useState(false);
 
   const searchFriends: SubmitHandler<SearchInput> = async (data) => {
+    setIsSearching(true);
     // Remove empty fields from search
     const filteredData = getFilteredData(data);
     console.log(filteredData)
     const allProfiles = await DataStore.query(UserProfile, (c) =>
-      c.or(() => criteria(c, filteredData))
+      c.and(() => criteria(c, filteredData))
     );
     // Filter out profiles with the same userProfile.id
     const otherProfiles = allProfiles.filter(
@@ -64,7 +68,41 @@ const FriendSearchModal = ({ isOpen, setIsOpen }: FriendSearchModalProps) => {
     });
     console.log(filteredResults)
     setResults(filteredResults);
+    setHasSearched(true);
+    setIsSearching(false);
   };
+
+  const renderResults = () => {
+    if (isSearching) {
+      return <p className="text-center">Searching for friends...</p>
+    }
+    if(results.length > 0) {
+      return (
+        <>
+          {results.map((result) => (
+            <FriendCard
+              key={result.id}
+              onClick={() => {
+                setCurrentResult(result);
+                setIsConfirmModalOpen(true);
+              }}
+              className="m-4 cursor-pointer"
+              profile={result}
+              link={false}
+            />
+          ))}
+        </>
+      )
+    }
+    if(hasSearched) {
+      return (
+        <p className="text-center">No results found</p>
+      )
+    }
+
+  }
+
+  console.log(isConfirmModalOpen)
 
   return (
     <>
@@ -75,40 +113,40 @@ const FriendSearchModal = ({ isOpen, setIsOpen }: FriendSearchModalProps) => {
           reset();
           setResults([]);
         }}
-        title="Friend Search"
+        title="Search for New Friends"
         className='max-w-6xl'
       >
         <form
           onSubmit={handleSubmit(searchFriends)}
           className="max-w-4xl p-4 mx-auto"
         >
-          <div className="flex flex-col md:flex-row justify-between">
-            <InputWrapper className="my-4 flex-1">
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+            <InputWrapper className="my-4 md:mx-4 flex-1">
               <Label>First Name</Label>
               <Input
                 {...register("firstName")}
                 type="text"
+                className='max-w-xl'
               />
             </InputWrapper>
-            <InputWrapper className="my-4 flex-1">
-            <Label>Last Name</Label>
-            <Input
-              {...register("lastName")}
-              type="text"
-            />
+            <InputWrapper className="my-4 md:mx-4 flex-1">
+              <Label>Last Name</Label>
+              <Input
+                {...register("lastName")}
+                type="text"
+                className='max-w-xl'
+              />
             </InputWrapper>
-          </div>
-          <div className="flex flex-col md:flex-row justify-between">
-            <InputWrapper className="my-4 flex-1">
+            <InputWrapper className="my-4 md:mx-4 flex-1">
               <Label>City</Label>
               <Input
                 {...register("city")}
                 type="text"
               />
             </InputWrapper>
-            <InputWrapper className="my-4 flex-1">
+            <InputWrapper className="my-4 md:mx-4 flex-1">
               <Label>State</Label>
-              <select
+              <Select
                 placeholder=""
                 {...register("state")}
                 name="state"
@@ -122,33 +160,23 @@ const FriendSearchModal = ({ isOpen, setIsOpen }: FriendSearchModalProps) => {
                     {state.name}
                   </option>
                 ))}
-              </select>
+              </Select>
             </InputWrapper>
-          </div>
-          <div className="flex flex-wrap">
-            <Label>School</Label>
-            <Input
-              {...register("school")}
-              type="text"
-            />
+            <InputWrapper className="my-4 md:mx-4 flex-1">
+              <Label>School</Label>
+              <Input
+                {...register("school")}
+                type="text"
+                className='max-w-xl'
+              />
+            </InputWrapper>
           </div>
           <div className='flex justify-center my-8'>
             <Button type="submit">Search</Button>
           </div>
         </form>
-        <div className="flex flex-wrap justify-center">
-          {results.map((result) => (
-            <FriendCard
-              key={result.id}
-              onClick={() => {
-                setCurrentResult(result);
-                setIsConfirmModalOpen(true);
-              }}
-              className="m-4 cursor-pointer"
-              profile={result}
-              link={false}
-            />
-          ))}
+        <div className="flex flex-wrap p-4 justify-center">
+          {renderResults()}
         </div>
       </Modal>
       <CustomAlert
@@ -174,7 +202,7 @@ const FriendSearchModal = ({ isOpen, setIsOpen }: FriendSearchModalProps) => {
           },
         ]}
       />
-      {toastData && (
+      { toastData && (
         <Toast
           toastData={toastData}
           onClose={() => {
