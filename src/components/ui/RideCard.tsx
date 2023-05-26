d468df1e06b5ea8af4e07f33611a29673f62dd4c
wzsamuels <wzsamuels@gmail.com>
2023-05-25 22:09:17 -0400

    Refactor api calls to ride services file

--- src/components/ui/RideCard.tsx
+++ src/components/ui/RideCard.tsx
@@ -6,6 +6,8 @@ import Button from "../common/Button/Button";
 import dayjs from "dayjs";
 import useProfileStore from "../../stores/profileStore";
 import useDataClearedStore from "../../stores/dataClearedStore";
+import CustomAlert from "../common/Alert/Alert";
+import {deleteRide, getRidersByEvent, joinRide, leaveRide} from "../../services/rideServices";
 
 interface RideCardProps {
   ride: Ride;
@@ -16,15 +18,14 @@ const RideCard = ({ ride, className }: RideCardProps) => {
   const [driverProfile, setDriverProfile] = useState<UserProfile>();
   const [passengerProfiles, setPassengerProfiles] = useState<UserProfile[]>();
   const [ridersCount, setRidersCount] = useState<number>(0);
+  const [deleteAlertOpen, setDeleteAlertOpen] = useState(false);
   const userProfile = useProfileStore((state) => state.userProfile);
   const dataCleared = useDataClearedStore((state) => state.dataCleared);
 
   useEffect(() => {
     if(!dataCleared) return;
     const getRiders = async () => {
-      const rideUsers = await DataStore.query(RideUser, (c) =>
-        c.rideID.eq(ride.id)
-      );
+      const rideUsers = await getRidersByEvent(ride);
       const driverUser = rideUsers.find((user) => user.isDriver === true);
       const driverProfile = await driverUser?.userProfile;
       const riderUsers = rideUsers.filter((user) => user.isDriver === false);
@@ -43,39 +44,37 @@ const RideCard = ({ ride, className }: RideCardProps) => {
   }, [ride]);
 
   const handleJoinRide = async () => {
-    if (userProfile && passengerProfiles) {
-      await DataStore.save(
-        new RideUser({
-          isDriver: false,
-          rideID: ride.id,
-          ride: ride,
-          userProfileID: userProfile.id,
-          userProfile: userProfile,
-        })
-      );
-      setRidersCount(ridersCount + 1);
-      setPassengerProfiles([...passengerProfiles, userProfile]);
-    } else {
-      alert("Please log in to join the ride");
-    }
+    if (!userProfile || !passengerProfiles) return;
+    await joinRide(ride, userProfile);
+    setRidersCount(ridersCount + 1);
+    setPassengerProfiles([...passengerProfiles, userProfile]);
   };
 
+  const handleLeaveRide = async () => {
+    if (!userProfile || !passengerProfiles) return;
+    await leaveRide(ride, userProfile);
+    setRidersCount(ridersCount - 1);
+    setPassengerProfiles(passengerProfiles.filter((passenger) => passenger.id !== userProfile.id));
+  }
+
   const renderButtons = () => {
-    const canJoin =
-      userProfile?.id !== driverProfile?.id &&
-      ridersCount < ride.maxPassengers &&
-      passengerProfiles?.find(
-        (passenger) => passenger.id === userProfile?.id
-      ) === undefined;
-    return (
-      <Button
-        className="mr-4 mt-4"
-        disabled={!canJoin}
-        onClick={handleJoinRide}
-      >
-        Join Ride
-      </Button>
-    );
+    if(userProfile?.id === driverProfile?.id) {
+      return (
+        <Button onClick={() => setDeleteAlertOpen(true)}>Cancel Ride</Button>
+      )
+    }
+
+    if(passengerProfiles?.find((passenger) => passenger.id === userProfile?.id)) {
+      return (
+        <Button onClick={handleLeaveRide}>Leave Ride</Button>
+      )
+    }
+
+    if (ridersCount < ride.maxPassengers) {
+      return (
+        <Button onClick={handleJoinRide}>Join Ride</Button>
+      );
+    }
   };
 
   return (
@@ -129,7 +128,28 @@ const RideCard = ({ ride, className }: RideCardProps) => {
           ))}
         </div>
       </div>
-      <div className="flex justify-center">{renderButtons()}</div>
+      <div className="flex justify-center m-4 gap-4">{renderButtons()}</div>
+      <CustomAlert
+        title="Alert Title"
+        message="Cancel this ride? This action cannot be undone."
+        isOpen={deleteAlertOpen}
+        setIsOpen={setDeleteAlertOpen}
+        buttons={[
+          {
+            text: "Cancel",
+            role: "cancel",
+          },
+          {
+            text: "OK",
+            role: "confirm",
+            handler: () => {
+              deleteRide(
+                ride.id,
+              );
+            },
+          },
+        ]}
+      />
     </div>
   );
 };
