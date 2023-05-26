54e7483e42942f20da7890feab5265231fcecca6
wzsamuels <wzsamuels@gmail.com>
2023-05-25 22:21:59 -0400

    Remove unused code, add unsubscribe function

--- src/stores/friendProfileStore.ts
+++ src/stores/friendProfileStore.ts
@@ -1,51 +1,30 @@
 import {create, SetState} from "zustand";
 import {Friendship, UserProfile} from "../models";
-import {API, graphqlOperation, GraphQLResult} from "@aws-amplify/api";
-import { listFriendships } from "../graphql/queries";
 import {DataStore} from "aws-amplify";
-
-type GraphQLResultExtended = GraphQLResult & {
-  data: {
-    listFriendships: {
-      items: Friendship[];
-    };
-  };
-  errors?: { message: string }[]
-};
 
 interface FriendStore {
   clearFriends: () => void;
   fetchFriends: (userProfile:  UserProfile) => void;
+  friendUnsubscribe: () => void;
   loadingFriends: boolean;
   allFriendships: Friendship[];
   acceptedFriendProfiles: UserProfile[];
   incomingFriendProfiles: UserProfile[];
   outgoingFriendProfiles: UserProfile[];
+  friendSubscription: any;
 }
 
-const useFriendStore = create<FriendStore>((set: SetState<FriendStore>) => ({
+const useFriendStore = create<FriendStore>((set: SetState<FriendStore>, get) => ({
   loadingFriends: false,
   allFriendships: [] as Friendship[],
   acceptedFriendProfiles: [] as UserProfile[],
   incomingFriendProfiles: [] as UserProfile[],
   outgoingFriendProfiles: [] as UserProfile[],
+  friendSubscription: null,
   clearFriends: () => set({acceptedFriendProfiles: [], incomingFriendProfiles: [], outgoingFriendProfiles: [], allFriendships: []}),
   fetchFriends: async (userProfile: UserProfile) => {
     set({ loadingFriends: true });
-    /*
-    const graphqlData = await API.graphql(graphqlOperation(listFriendships, {
-      filter: {
-        or: [
-          { userProfileID: { eq: userProfile.id } },
-          { friendProfileID: { eq: userProfile.id } },
-        ],
-        and: [
-          { isAccepted: { eq: true } }
-        ]
-      }
-    })) as GraphQLResultExtended;
-    const items = graphqlData.data.listFriendships.items;
-     */
+
     const friendshipSub = DataStore.observeQuery(Friendship, (c) =>
       c.or((c) => [
         c.userProfileID.eq(userProfile.id),
@@ -76,18 +55,14 @@ const useFriendStore = create<FriendStore>((set: SetState<FriendStore>) => ({
       );
 
       set({ allFriendships: items });
-
       set({ acceptedFriendProfiles: acceptedFriends });
       set({ incomingFriendProfiles: incomingFriendRequests });
       set({ outgoingFriendProfiles: outgoingFriendRequests });
       set({ loadingFriends: false });
     });
-
-    return () => {
-      friendshipSub.unsubscribe();
-    };
-  }
-
+    set({ friendSubscription: friendshipSub });
+  },
+  friendUnsubscribe: () => get().friendSubscription.unsubscribe()
 }));
 
 export default useFriendStore;
