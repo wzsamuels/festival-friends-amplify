b28ede41e009eda690ee855186d30d5ca58a2e9e
wzsamuels <wzsamuels@gmail.com>
2023-05-27 22:16:56 -0400

    Add photo services function for retrieving a profile's photo

--- src/services/PhotoServices.ts
+++ src/services/PhotoServices.ts
@@ -1,17 +1,7 @@
 import {Auth, Storage} from "aws-amplify";
 import {v4 as uuidv4} from "uuid";
-import {GraphQLResult} from "@aws-amplify/api";
 import {Photo, UserProfile} from "../models";
 import {DataStore} from "@aws-amplify/datastore";
-
-// TODO: Add error handling
-
-type GraphQLResultExtended = GraphQLResult & {
-  data: {
-    createPhoto: Photo
-  };
-  errors?: { message: string }[]
-};
 
 export const createNewPhoto = async (sub: string, photoFile: File, profile: UserProfile) => {
   const id = uuidv4();
@@ -39,20 +29,15 @@ export const createNewPhoto = async (sub: string, photoFile: File, profile: User
     console.log("Error saving photo:", e);
     return null;
   }
+}
 
-  /*
-  const photoData = await API.graphql(graphqlOperation(createPhoto, {
-    input: {
-      s3Key: `${sub}/${id}`,
-      isPrivate: false,
-      userProfileID: profileID,
-      identityId: identityId
-    }
-  })) as GraphQLResultExtended;
-
-  return photoData.data.createPhoto;
-
-   */
-
-
+export const getPhotosByProfile = async (profile: UserProfile) => {
+  try {
+    return await DataStore.query(Photo, (c) =>
+      c.userProfileID.eq(profile.id)
+    );
+  } catch (e) {
+    console.log("Error getting photos:", e);
+    return [];
+  }
 }
