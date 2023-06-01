4f15b9273b79759c807208d188897bb3a423a77b
wzsamuels <wzsamuels@gmail.com>
2023-05-31 23:24:05 -0400

    Allow search icon when not signed in

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,7 +15,7 @@ const Header = ({ children, onSearch }: HeaderProps) => {
     <header className="bg-white shadow-xl w-full flex items-center fixed  top-0 z-10">
       <div className="flex-1 w-full">{children}</div>
       <div className="[&>*]:p-2 [&>*]:md:p-4 flex">
-        {route === "authenticated" && onSearch ? (
+        {onSearch ? (
           <SearchButton onClick={onSearch} />
         ) : null}
         <AccountButton />
