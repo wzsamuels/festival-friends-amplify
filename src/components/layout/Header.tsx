695edf40c6ce7cebd0c20449dce3da9ec4ccaaee
wzsamuels <wzsamuels@gmail.com>
2023-06-09 19:48:12 -0400

    Adjust header styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AccountButton from "../ui/AccountButton";
 import SearchButton from "../ui/SearchButton";
 import { useAuthenticator } from "@aws-amplify/ui-react";
+import {Link} from "react-router-dom";
 
 interface HeaderProps {
   children?: React.ReactNode;
@@ -13,13 +14,18 @@ const Header = ({ children, onSearch }: HeaderProps) => {
 
   return (
     <header className="bg-white shadow-xl w-full flex items-center fixed  top-0 z-10">
+      <Link to="/" className="h-10 w-10 m-1 bg-lightYellow flex justify-center items-center rounded-full font-bold">
+        EF
+      </Link>
       <div className="flex-1 w-full md:mr-4">{children}</div>
-      <div className="[&>*]:p-2 [&>*]:md:p-4 flex">
-        {onSearch ? (
-          <SearchButton onClick={onSearch} />
-        ) : null}
+        {onSearch &&
+          <div className="mx-1 sm:mx-4 md:mx-4 "><SearchButton onClick={onSearch} />
+          </div>
+        }
+      <div className="mr-2 sm:mx-4 md:mx-4 ">
         <AccountButton />
       </div>
+
     </header>
   );
 };
