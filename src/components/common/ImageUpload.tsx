d929c80a2720ea597017c33da508b18afb662897
wzsamuels <wzsamuels@gmail.com>
2023-06-09 19:57:35 -0400

    Fix reused IDs in ImageUpload component

--- src/components/common/ImageUpload.tsx
+++ src/components/common/ImageUpload.tsx
@@ -1,17 +1,18 @@
-import React, {Dispatch, SetStateAction} from "react";
-
+import React, {Dispatch, SetStateAction, useRef, useState} from "react";
+import {v4 as uuidv4} from "uuid";
 const ImageUpload = ({setSelectedFile} : {setSelectedFile : Dispatch<SetStateAction<File | null>>}) => {
+  const id = useRef<string>(uuidv4());
   return (
     <>
       <label
-        htmlFor="upload-profile-image-photo"
-        className="my-4 cursor-pointer flex justify-center p-4 border bg-green-950 text-white border-primary-default"
+        htmlFor={`upload-profile-image-photo-${id.current}`}
+        className="my-4 cursor-pointer flex justify-center p-4 border bg-lightYellow"
       >
         Upload New Photo
       </label>
       <input
         type="file"
-        id="upload-profile-image-photo"
+        id={`upload-profile-image-photo-${id.current}`}
         accept="image/png, image/jpeg"
         onChange={(e) =>
           e?.target?.files && setSelectedFile(e.target.files[0])
