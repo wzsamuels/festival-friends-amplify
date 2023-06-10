993675925435e8a67fa8d9fd3d080bb1b463e841
wzsamuels <wzsamuels@gmail.com>
2023-06-10 18:59:26 -0400

    Fixes to event form styles

--- src/components/pages/AdminPage/EventForm.tsx
+++ src/components/pages/AdminPage/EventForm.tsx
@@ -1,4 +1,3 @@
-import Label from "../../common/Label/Label";
 import Input from "../../common/Input/Input";
 import Select from "../../common/Select";
 import states from "../../../data/states";
@@ -15,13 +14,15 @@ import ImageContext from "../../../context/ImageContext";
 import {getEvent} from "../../../services/eventServices";
 
 interface EventFormProps {
-  eventID: string;
-  onSubmit: SubmitHandler<EventInputs>
+  eventID?: string;
+  onSubmit: SubmitHandler<EventInputs>;
+  submitting: boolean;
+  className?: string;
 }
 
 const eventTypes = ["SPORT", "BUSINESS", "MUSIC", "TRAVEL", "COLLEGE"];
 
-const EventForm = ({eventID, onSubmit} : EventFormProps) => {
+const EventForm = ({eventID, onSubmit, submitting, className} : EventFormProps) => {
   const [event, setEvent] = useState<Event>();
   const [eventImage, setEventImage] = useState("");
   const { preview, selectedFile, setSelectedFile } = useFilePreview();
@@ -34,7 +35,9 @@ const EventForm = ({eventID, onSubmit} : EventFormProps) => {
     register("selectedFile",{ required: true});
   }, [])
 
-  useEffect(() =>{
+  useEffect(() => {
+    if(!eventID) return;
+
     try {
       getEvent(eventID).then(event => setEvent(event as Event))
     } catch(e) {
@@ -74,25 +77,29 @@ const EventForm = ({eventID, onSubmit} : EventFormProps) => {
   }, [selectedFile]);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='p-4 max-w-xl w-full'>
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>Name</Label>
-        <Input {...register("name",  {required: true})}/>
+    <form onSubmit={handleSubmit(onSubmit)} className={`p-4 max-w-xl w-full ${className}`}>
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">Name</label>
+        <input
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0"
+          {...register("name",  {required: true})}/>
       </div>
       {errors.name && <p className='text-red-500 text-sm'>This field is required</p>}
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>Genre / Category</Label>
-        <Input
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">Genre / Category</label>
+        <input
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0"
           placeholder="Ex. Rock, Beach Trip, Business Conference"
           {...register("genre",  {required: true})}/>
       </div>
         {errors.genre && <p className='text-red-500 text-sm'>This field is required</p>}
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>State</Label>
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">State</label>
         <Select
           {...register("state", { required: true })}
           name="state"
           defaultValue=""
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0 max-w-[10rem]"
         >
           <option value="" disabled>
             Select state
@@ -105,52 +112,64 @@ const EventForm = ({eventID, onSubmit} : EventFormProps) => {
         </Select>
       </div>
       {errors.state && <p className='text-red-500 text-sm'>This field is required</p>}
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>City</Label>
-        <Input {...register("city",  {required: true})}/>
+
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">City</label>
+        <input
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0"
+          {...register("city",  {required: true})}/>
       </div>
       {errors.city && <p className='text-red-500 text-sm'>This field is required</p>}
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>Address</Label>
-        <Input {...register("address")}/>
+
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">Address</label>
+        <input
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0"
+          {...register("address")}/>
       </div>
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>Start Date</Label>
-        <Input {...register("startDate",  {required: true})} type="date"/>
+
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">Start Date</label>
+        <input
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0 max-w-[10rem]"
+          {...register("startDate",  {required: true})} type="date"/>
       </div>
       {errors.startDate && <p className='text-red-500 text-sm'>This field is required</p>}
 
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>End Date</Label>
-        <Input {...register("endDate",  {required: true})} type="date"/>
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">End Date</label>
+        <input
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0 max-w-[10rem]"
+          {...register("endDate",  {required: true})} type="date"/>
       </div>
       {errors.endDate && <p className='text-red-500 text-sm'>This field is required</p>}
 
       <h2 className="my-4">Event Image</h2>
-          {
-            preview || eventImage ?
-              <img src={preview || eventImage || ""} alt="Preview" className="w-full h-full" />
-              :
-              <div className="w-full h-full text-center min-h-[24rem] bg-gray-500 relative">
-                <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-2xl">No image selected</span>
-              </div>
-          }
-        <div className="mt-6 flex justify-center w-full">
-          <ImageUpload setSelectedFile={setSelectedFile}/>
-        </div>
-        {errors.selectedFile && <p className='text-red-500'>A Photo is required</p>}
+      {
+        preview || eventImage ?
+          <img src={preview || eventImage || ""} alt="Preview" className="w-full h-full" />
+          :
+          <div className="w-full h-full text-center min-h-[24rem] bg-gray-500 relative">
+            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-2xl">No image selected</span>
+          </div>
+      }
+      <div className="mt-6 flex justify-center w-full">
+        <ImageUpload setSelectedFile={setSelectedFile}/>
+      </div>
+      {errors.selectedFile && <p className='text-red-500'>A Photo is required</p>}
 
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>Description</Label>
-        <TextArea {...register("description",  {required: true})}/>
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">Description</label>
+        <TextArea className="mt-2" {...register("description",  {required: true})}/>
       </div>
 
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>Type</Label>
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">Type</label>
         <Select
           {...register("type", { required: true })}
           name="type"
           defaultValue=""
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0 max-w-[10rem]"
         >
           <option value="" disabled>
             Select event type
@@ -162,12 +181,24 @@ const EventForm = ({eventID, onSubmit} : EventFormProps) => {
           ))}
         </Select>
       </div>
-      <div className='flex flex-wrap mt-4 mb-1'>
-        <Label>Website</Label>
-        <Input {...register("url")}/>
+      {errors.type && <p className='text-red-500 text-sm'>This field is required</p>}
+
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">Website Link</label>
+        <input
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0"
+          {...register("url")}/>
       </div>
+      <div className='flex flex-col sm:flex-row mt-4 mb-1'>
+        <label className="sm:basis-[150px]">Ticket Link</label>
+        <input
+          className="border-b border-b-gray-400 flex-1 focus:border-b-brandYellow focus:outline-0 mt-2 sm:m-0"
+          {...register("ticketURL")}/>
+      </div>
+
+
       <div className='flex justify-center my-6'>
-        <Button type="submit">Create New Event</Button>
+        <Button type="submit" className="disabled:opacity-50" disabled={submitting}>{submitting ? "Submitting..." : " Submit"}</Button>
       </div>
     </form>
   )
