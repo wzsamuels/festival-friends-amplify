a9035a3bc0aa3422f9632d441ccd1cdebc53be73
wzsamuels <wzsamuels@gmail.com>
2023-06-10 19:01:05 -0400

    Add group checking to admin pages

--- src/components/pages/AdminPage/AdminLayout.tsx
+++ src/components/pages/AdminPage/AdminLayout.tsx
@@ -1,8 +1,32 @@
 import Header from "../../layout/Header";
 import {Link, Outlet} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useAuthenticator} from "@aws-amplify/ui-react";
+import {Auth} from "aws-amplify";
 
 const AdminLayout = () => {
+  const [group, setGroup] = useState("")
+  const { route } = useAuthenticator((context) => [context.route]);
+
+  console.log(group)
+
+  useEffect(() => {
+    if(route !== "authenticated") return;
+    Auth.currentAuthenticatedUser()
+      .then(user => {
+        setGroup(user.signInUserSession.accessToken.payload["cognito:groups"].find((group: string) => group === "admin")?.toString() ?? "")
+      })
+  }, [route])
+
+
+  if (group !== "admin") {
+    return (
+      <div>
+        <h1>You are not authorized to view this page</h1>
+      </div>
+    )
+  }
+
   return (
     <>
       <Header>
