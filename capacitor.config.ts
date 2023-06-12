302f0e862a64c00fc29e419f8969d0a3314bd6a3
wzsamuels <wzsamuels@gmail.com>
2023-06-12 19:31:51 -0400

    Update capacitor.config.ts for iOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,13 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.twinsilverdesign.www",
+  appId: "app.eventfriends.www",
   appName: "Event Friends",
   webDir: "dist",
   bundledWebRuntime: false,
+  "ios": {
+    "contentInset": "always"
+  }
 };
 
 export default config;
