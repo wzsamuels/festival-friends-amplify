cea752e28db88b3961c56e8826dbeea9851310d9
wzsamuels <wzsamuels@gmail.com>
2023-06-11 21:12:42 -0400

    Add unique ID for nav specific classnames

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import {NavLink, useLocation} from "react-router-dom";
 import {NavItem} from "../../types";
+import {v4 as uuidv4} from "uuid";
 
 interface NavProps {
   items: NavItem[];
@@ -11,10 +12,11 @@ const Nav = ({items, className} : NavProps) => {
   const underlineRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
   const activePath = location.pathname;
+  const id = useRef<string>(uuidv4());
 
   const updateUnderlinePosition = () => {
-    const activeLink = document.querySelector(`.nav-link[href='${activePath}']`);
-    const parent = document.querySelector('.nav-container');
+    const activeLink = document.querySelector(`.nav-link-${id.current}[href='${activePath}']`);
+    const parent = document.querySelector(`.nav-container-${id.current}`);
 
     if (underlineRef.current && activeLink && parent) {
       const linkRect = activeLink.getBoundingClientRect();
@@ -42,11 +44,11 @@ const Nav = ({items, className} : NavProps) => {
   }, []);
 
   return (
-    <div className={`w-full flex justify-between h-full relative flex-wrap ${className} nav-container`}>
+    <div className={`w-full flex justify-between h-full relative flex-wrap ${className} nav-container-${id.current}`}>
       {items.map((item, index) => (
         <NavLink
           key={index}
-          className="nav-link relative flex-1 py-4 px-1 sm:px-2 md:px-4 text-sm sm:text-base flex justify-center"
+          className={`nav-link-${id.current} relative flex-1 py-4 px-1 sm:px-2 md:px-4 text-sm sm:text-base flex justify-center`}
           to={item.link}
         >
           {item.content}
