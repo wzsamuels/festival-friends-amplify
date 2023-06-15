8db322162b23034f06fdb8ff984cc65624bd2112
wzsamuels <wzsamuels@gmail.com>
2023-06-14 22:53:29 -0400

    Add capacitor asset generator and vite PWA dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
