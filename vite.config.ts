5bce890805bc4b19005e82d49e96a752e1236cd7
wzsamuels <wzsamuels@gmail.com>
2023-06-19 20:04:24 -0400

    Add feature to report users

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    react()
   ],
   optimizeDeps: {
     esbuildOptions: {
