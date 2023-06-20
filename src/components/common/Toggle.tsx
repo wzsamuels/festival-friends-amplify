0893f4a96a46f84cce44ec11d3889b6f18b5c95e
wzsamuels <wzsamuels@gmail.com>
2023-06-19 20:48:26 -0400

    Remove unused "classnames" package, add polyfill for addEventListener on old Safari

--- src/components/common/Toggle.tsx
+++ src/components/common/Toggle.tsx
@@ -1,6 +1,5 @@
 import {Control, FieldPath, FieldValues, useController} from "react-hook-form";
 import {Switch} from "@headlessui/react";
-import classNames from "classnames";
 import React from "react";
 
 interface ToggleProps<T extends FieldValues> {
@@ -28,17 +27,11 @@ const Toggle = <T extends FieldValues,>({name, control, label}: ToggleProps<T>) 
       <Switch
         checked={value}
         onChange={onChange}
-        className={classNames(
-          value ? "bg-darkGreen" : "bg-darkYellow",
-          "relative mr-8 inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
-        )}
+        className={`${value ? "bg-darkGreen" : "bg-darkYellow"} relative mr-8 inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black`}
       >
         <span
           aria-hidden="true"
-          className={classNames(
-            value ? "translate-x-5" : "translate-x-0",
-            "pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-          )}
+          className={`${value ? "translate-x-5" : "translate-x-0"} pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
         />
       </Switch>
     </Switch.Group>
