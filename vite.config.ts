9690dacfc675acd6bd338f6c624584cf3bbc6d50
wzsamuels <wzsamuels@gmail.com>
2023-06-23 17:55:59 -0400

    Add manual code chunking for vite

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,13 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      output:{
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      },
       plugins: [
         // Enable rollup polyfills plugin
         // used during production bundling
