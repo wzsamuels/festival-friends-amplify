52b59bf68a243436d57a0f848ae67b291b566664
wzsamuels <wzsamuels@gmail.com>
2023-07-16 19:12:31 -0400

    Add function for calculating distance in miles between GPS coordinates

---
+++ src/lib/calculateDistance.ts
@@ -0,0 +1,17 @@
+function calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number) {
+  const R = 3958.8; // Radius of the Earth in miles
+  const lat1Rad = lat1 * Math.PI / 180; // Convert degrees to radians
+  const lat2Rad = lat2 * Math.PI / 180;
+  const deltaLat = (lat2 - lat1) * Math.PI / 180;
+  const deltaLon = (lon2 - lon1) * Math.PI / 180;
+
+  const a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+    Math.cos(lat1Rad) * Math.cos(lat2Rad) *
+    Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c;
+}
+
+export default calculateDistance;
