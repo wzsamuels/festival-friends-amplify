89305765d9a7b8a5307f30da3087a3e9fd6cd599
wzsamuels <wzsamuels@gmail.com>
2023-07-16 21:27:06 -0400

    Add amplify configuration for oauth

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,41 @@ import awsExports from "./aws-exports";
 import Amplify from "@aws-amplify/core";
 import {AuthModeStrategyType} from "@aws-amplify/datastore";
 
-Amplify.configure({
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === "[::1]" ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+);
+
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [
+  productionRedirectSignIn,
+  localRedirectSignIn,
+] = awsExports.oauth.redirectSignIn.split(",");
+
+const [
+  localRedirectSignOut,
+  productionRedirectSignOut,
+] = awsExports.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = {
   ...awsExports,
+  oauth: {
+    ...awsExports.oauth,
+    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+  },
   DataStore: {
     authModeStrategyType: AuthModeStrategyType.MULTI_AUTH,
   },
-});
+}
+
+Amplify.configure(updatedAwsConfig);
+
 const App = () => {
 
   return (
