38cbefcc4340ba86c79bb545449724a68e026610
wzsamuels <wzsamuels@gmail.com>
2023-08-09 10:40:36 -0400

    Add auth provider for Auth0 around app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import {Auth0Provider} from "@auth0/auth0-react";
+import { domain as auth0Domain, clientId, callbackUri } from "./auth.config";
+
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={auth0Domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: callbackUri
+      }}
+      // For using Auth0-React with Ionic on Android and iOS,
+      // it's important to use refresh tokens without the fallback
+      useRefreshTokens={true}
+      useRefreshTokensFallback={false}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
