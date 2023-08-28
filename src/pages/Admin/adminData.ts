bcd0c4546bff9fddb551d83167b1244738aa790c
wzsamuels <wzsamuels@gmail.com>
2023-08-27 23:41:20 -0400

    Add data file for admin nav links

---
+++ src/pages/Admin/adminData.ts
@@ -0,0 +1,10 @@
+export const adminEventNavItems = [
+  { content: "Create", link: '/admin/events/create'},
+  { content: "Update", link: "/admin/events/update"},
+  { content: "Delete", link: "/admin/events/delete"}
+]
+
+export const adminNavItems = [
+  { content: "Events", link: "/admin/events" },
+  { content: "Accounts", link: "/admin/accounts" }
+]
